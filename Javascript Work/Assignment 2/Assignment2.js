95e1fdd187ba5865aeb92fcd31efbd02d694e33c
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2022-09-28 10:29:50 +0100

    Add files via upload

--- Javascript Work/Assignment 2/Assignment2.js
+++ Javascript Work/Assignment 2/Assignment2.js
@@ -1,6 +1,6 @@
 // 1 
 
-let number1 = Number(prompt("Enter a number"));
+let number1 = Number(prompt("Q1: Enter a number"));
 
 number1 %= 2;
 
@@ -13,7 +13,7 @@ else {
 
 // 2
 
-let number2 = Number(prompt("Enter a number"));
+let number2 = Number(prompt("Q2: Enter a number"));
 let number3 = Number(prompt("Enter another number"));
 
 if(number2>number3){
@@ -28,7 +28,7 @@ else{
 
 // 3
 
-let number4 = Number(prompt("Enter a number"));
+let number4 = Number(prompt("Q3: Enter a number"));
 let number5 = Number(prompt("Enter another number"));
 let number6 = Number(prompt("Enter another number"));
 
@@ -44,7 +44,7 @@ else{
 
 // 4
 
-let side1 = Number(prompt("Enter one side of a triangle"));
+let side1 = Number(prompt("Q4: Enter one side of a triangle"));
 let side2 = Number(prompt("Enter another side of the triangle"));
 let side3 = Number(prompt("Enter the last side of the triangle "));
 
@@ -58,4 +58,35 @@ else{
     console.log("Your triangle is scalene")
 }
 
-//5
+//5 
+
+let digit51 = Number(prompt("Q5:Enter a number"));
+
+let digit52 = Number(prompt("Enter another number"));
+
+let useri = prompt("Please enter add,subtract,multiply,divide or modulus.")
+
+if(useri=="add"){
+    let if1 = digit51+digit52
+    console.log("Your numbers added are", if1 )
+   }
+   else if(useri=="subtract"){
+    let if2 = digit51-digit52
+    console.log("Your numbers subtracted are",if2)
+   }
+   else if(useri=="multiply"){
+    let if3 = digit51*digit52
+    console.log("Your numbers multiplied are",if3)
+}
+   else if(useri=="divide"){
+    let if4 = digit51/digit52
+    console.log("Your numbers divided are",if4)
+    }
+    else if(useri=="modulus"){
+    let if5 = digit51%digit52
+    console.log("Your numbers modulus is",if5)
+    }
+   else{
+       console.log(useri,"Is not a valid input")
+   }
+   
