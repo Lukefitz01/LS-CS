d8d5fd91be34a3546571b595810518b7626e5c43
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2022-10-12 10:54:42 +0100

    Add files via upload

---
+++ Javascript Work/Exercise 6/Exercise6.js
@@ -0,0 +1,42 @@
+ // Q1
+ /*let count=1;
+while(count<=10){
+  console.log(count);
+  count++; 
+}
+*/
+//Q2
+/*let count2=2;
+while(count2<=98){
+ console.log(count2);
+  count2++; 
+  count2++;
+}*/
+
+//Q3
+
+/*let num = Number(prompt("Q3:Enter a number"));
+let num2 = Number(prompt("Q3:Enter a power"));
+let result = 1
+
+while (num2 != 0) {
+    result = result * num;
+    num2--;
+}
+console.log(result)
+*/
+
+//Q4 
+
+let q4 = Number(prompt("Q4:Enter a number"));
+let one = 0
+let star = "*"
+let x = star
+while (one != q4){
+    one++
+    console.log(star)
+    star = star + x
+   
+}
+
+// Q5
