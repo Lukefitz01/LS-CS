ef47ba92b180c6b0fd0e835f239b5ae6eb7ab710
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2022-10-17 18:25:14 +0100

    Add files via upload

--- LS-CS-main/Javascript Work/Exercise 7 For loops/Exercise7.js
+++ LS-CS-main/Javascript Work/Exercise 7 For loops/Exercise7.js
@@ -1,8 +1,8 @@
 // Q1
-
+/*
 let num = Number(prompt("Q1:Enter a number"));
 let n2 = Math.ceil(num / 2)
-for(let i = 1;i<n2;n2++ ){
+for(let i = 1;i<n2 + 1;n2++ ){
     
    
     console.log(i);
@@ -10,3 +10,15 @@ for(let i = 1;i<n2;n2++ ){
     
     
 }
+*/
+//Q2
+
+let num2 = Number(prompt("Q2:Enter a number"));
+let n3 = 1
+for(let i = 1;i<num2;){
+  
+   n3 = n3 * num2 
+   
+   num2--
+}
+console.log(n3)
