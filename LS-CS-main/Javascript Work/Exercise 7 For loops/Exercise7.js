62bdd1af86c8e6ea2897a06d32651b4ec5658bc0
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2022-10-19 10:25:45 +0100

    Add files via upload

--- LS-CS-main/Javascript Work/Exercise 7 For loops/Exercise7.js
+++ LS-CS-main/Javascript Work/Exercise 7 For loops/Exercise7.js
@@ -1,5 +1,5 @@
 // Q1
-/*
+
 let num = Number(prompt("Q1:Enter a number"));
 let n2 = Math.ceil(num / 2)
 for(let i = 1;i<n2 + 1;n2++ ){
@@ -10,7 +10,7 @@ for(let i = 1;i<n2 + 1;n2++ ){
     
     
 }
-*/
+
 //Q2
 
 let num2 = Number(prompt("Q2:Enter a number"));
@@ -22,3 +22,39 @@ for(let i = 1;i<num2;){
    num2--
 }
 console.log(n3)
+
+// Q3
+
+
+let number = 75897589
+
+for(let i = 1;i<5;i++){
+let guess =  Number(prompt("Q3:Enter an 8 digit number"));
+
+if(guess == number){
+    console.log("That is correct")
+    break;
+    
+}
+else{
+    console.log("That is incorrect")
+}
+
+}
+
+
+//Q4
+
+let userinput =  Number(prompt("Q4: Enter a number"));
+let n4 = 0
+let o = userinput +1
+for(let i = 1;i < o;i++){
+
+n4 = n4 + i
+    if(userinput < 1){
+        console.log("Invalid Input")
+        break;
+    }
+}
+
+console.log(n4)
