73d6cf68f29870bda58ea2a0233a55b141d827f9
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2022-11-18 12:16:10 +0000

    Add files via upload

--- LS-CS-main (1)/Javascript Work/Exercise11_Arrays/Exercise11.js
+++ LS-CS-main (1)/Javascript Work/Exercise11_Arrays/Exercise11.js
@@ -3,7 +3,7 @@
 
 x = myarray2.length
 //Q2
-/*
+
 for(let i = 0; i<x; i++){
     document.write(myarray2[i])
     document.write("<br>")
@@ -45,7 +45,7 @@ for(let i = 0; i<x; i++){
 
 }
 document.write(temp3,"<br>",temp4)
-*/
+
 //Q6
 
 let temp5 = myarray2[0]
@@ -75,26 +75,85 @@ document.write(temp6)
 
 //Q7
 let temp7 = 0
+let temp8 = 0
+let temp9 = 0
+let temp10 = 0
 let newarray = []; 
 let newarray2 = [];
 let newarray3 = [];
-let y = newarray.length;
-let c = newarray2.length;
-let v = newarray3.length;
+let y = newarray.length;+ 1;
+let c = newarray2.length + 1;
+let v = newarray3.length + 1;
+for(let i = 0; i<x; i++){
+temp10 = myarray2[i]
+    if (temp10 == 0 ) {
+        
+        temp7 = temp10
+        console.log(temp7)
+        for(let x = 0; x<y;x++) {
+        newarray[x] = temp7
+        }
+        y = newarray.length + 1;
+      } 
+    else if(temp10 % 2 == 0){
+       
+        temp8 = temp10
+        for(let z = 0; z<c; z++){
+        newarray2[z] = temp8 
+        }
+        c = newarray2.length + 1;
+     }
+    else{
+        
+        temp9 = temp10 
+        for(let a = 0; a<v ; a++){
+        newarray3[a] = temp7 
+        }
+        v = newarray3.length + 1;
+    }
+    
+   temp7 = 0
+   temp8 = 0
+   temp9 = 0
+   temp10 = 0
+    
+}
+
+console.log("There are",newarray2.length,"even elements in the array")
+console.log("There are",newarray3.length,"odd elements in the array")
+
+
+
+
+//Q8 
+let newarray4 = []; 
+let temp11 = 0
+
 for(let i = 0; i<x; i++){
 
-    if (myarray2[i] % 2 == 0) {
-        temp7 = myarray2[i]
-        for(let z = 0; z<y; z++)
-        newarray[i] = temp7 
-        
-      } 
-    else if(myarray2[i] == 0){
-        temp7 = myarray2[i]
-        newarray2[i]
-    }
-    else{
+newarray4[i] = myarray2[i]
+    
+}
+let xx = newarray4.length 
+for(let qq = 0; qq<xx; qq++){
+    console.log(newarray4[qq])
+    
+}
 
-    }
+//Q9
 
+myarray2.push(3)
+let xx = myarray2.length
+for(let qq = 0; qq<xx; qq++){
+    console.log(myarray2[qq])
+    
+}
+
+//Q10
+
+myarray2.pop();
+let xy = myarray2.length
+for(let qq = 0; qq<xy; qq++){
+    console.log(myarray2[qq])
+    
 }
