c55993cc6b1ee84bd909a68cdbf0c05962edf83b
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2023-01-13 12:33:36 +0000

    Add files via upload

--- LS-CS-main (1)/Javascript Work/Functions 15/functions15.js
+++ LS-CS-main (1)/Javascript Work/Functions 15/functions15.js
@@ -19,7 +19,7 @@ console.log(q1(array, count, ns , y , temp))
 
 
 // Q2 
-let array2 = [1, 7 , 9 , 9, 9 , 7]
+let array2 = [1, 7 ,7,  9 , 9, 9 , 7]
 let ns2 = ""
 let count1 = 0
 let count2 = 0
@@ -34,7 +34,8 @@ let count0 = 0
 let yy = array2.length
 let temp2 = 0
 let temp3 = 0
-function q2(array2, ns2 , yy , temp2 ,temp3,  count0 ,count1 , count2 , count3 , count4 , count5 , count6 , count7 , count8 , count9)
+let modes = [];
+function q2( array2 , ns2 , yy , temp2 ,temp3,  count0 ,count1 , count2 , count3 , count4 , count5 , count6 , count7 , count8 , count9)
 {
     for (let i = 0; i < yy ; i++) {
         ns2 = array2[i]
@@ -65,7 +66,7 @@ function q2(array2, ns2 , yy , temp2 ,temp3,  count0 ,count1 , count2 , count3 ,
         count7 = count7 + 1
        }
        else if(temp2 == 8){
-        count8 = count08+ 1
+        count8 = count8 + 1
        }
        else if(temp2 == 9 ){
         count9 = count9 + 1
@@ -82,10 +83,55 @@ function q2(array2, ns2 , yy , temp2 ,temp3,  count0 ,count1 , count2 , count3 ,
         if (array22[j] > temp3) {
             temp3 = array22[j];
            // console.log(temp3)
+           modes = []
+           modes.push(j);
           } 
-        
+
+        else if(array22[j] == temp3){
+            modes.push(j);
         }
-    return temp3;
-}
+        }
+    
+    return modes; 
+
+    }
 console.log(q2(array2, ns2 , yy , temp2, temp3, count0 ,count1 , count2 , count3 , count4 , count5 , count6 , count7 , count8 , count9))
 
+
+// Q3
+let array3 = [1, 7 , 9 , 7 ,45 ,6 , 9]
+let w = array3.length
+let temp4 = 0
+let q = 0
+let a = 0
+let s = 0
+function q3(array3, w , q ,a ,s)
+{
+array3.sort();
+// console.log(array3[0,1,2,3,4,5,6,7,8]);
+if(w % 2 == 0){
+//Even
+let ww = w / 2
+    for (let t = 0; t < ww; t++) {
+        if( t == ww - 1 ){
+        // return a and s as here and there
+        }
+        else if(t = ww - 2 ){
+
+
+
+        }
+        
+    }  
+
+}
+else{
+//Odd
+    let ww = Math.floor(w/2)
+    q = array3[ww];
+    return q;
+
+}
+
+}
+console.log(q3(array3 , w , temp , a ,s ))
