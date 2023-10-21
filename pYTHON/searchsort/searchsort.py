e74ef086f6779eed194d0fb700bc23442d6d9efd
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2023-10-21 15:44:56 +0100

    Add files via upload

--- pYTHON/searchsort/searchsort.py
+++ pYTHON/searchsort/searchsort.py
@@ -36,16 +36,86 @@ def binary1(bininp):
     binary.sort()
     low = 0
     high = len(binary) - 1
-    print(low,high)
-    mid1 = low + high
-    mid = mid1 // 2
+    #print(low,high)
+   
     while 1 > 0:
+        mid1 = low + high
+        mid = mid1 // 2
         if binary[mid] == bininp:
-            return mid 
+            return mid
+            break
+           
+            
         elif binary[mid] < bininp:
             low = mid + 1
+          
+            
         elif binary[mid] > bininp:
             high = mid - 1
+           
+            
     
 c = binary1(bininp)
 print(c)
+
+def binrec(low,high,binary,bininp2):
+        mid1 = low + high
+        mid = mid1 // 2
+        print(low,high,mid)
+        print(bininp2)
+        if binary[mid] == bininp2:
+            print("ENDING IS",mid)
+            return mid 
+            
+        elif binary[mid] < bininp2:
+            low = mid + 1
+            binrec(low,high,binary,bininp2)
+          
+        elif binary[mid] > bininp2:
+            high = mid - 1
+            binrec(low,high,binary,bininp2)
+            
+bininp2 = int(input("give a search argument for binary rec : "))
+def binary2(bininp2):
+    binary = [2,4,14,7,8,9,12,5,17,19,22,25,37,28,33,28]
+    binary.sort()
+    low = 0
+    high = len(binary) - 1
+   
+    cc = binrec(low,high,binary,bininp2)
+    print("cc",cc)
+    return(cc)
+           
+            
+    
+c2 = binary2(bininp2)
+print(c2)
+
+
+def simplesort():
+    list1 = [1,22,764,2,90]
+    list2 = []
+    value = 0
+    y = 0
+    y2 = 999
+    lenL = len(list1) 
+    for u in range(lenL):
+        y = 0
+        y2 = 999
+        for x in list1:
+            
+            if y == 0:
+               value = list1[0]
+               y2 = y
+            elif list1[y] < value:
+                value = list1[y]
+                y2 = y
+            y = y + 1
+          
+        list2.append(value)
+        list1.pop(y2)
+          
+    return list2
+v = simplesort()
+print(v)
+    
