c7609531cfe04dd070a56c889612898c29dec23b
Lukefitz01 <112865050+Lukefitz01@users.noreply.github.com>
2024-04-26 14:30:38 +0100

    Add files via upload

--- revision3.py
+++ revision3.py
@@ -1,27 +1,53 @@
 f = open("1000 random numbers - corrupted 1.txt","r")
 alexander = []
 y = f.readline()
-print(y)
+#print(y)
 
 # print(type(y))
 
 lenny = len(y)
-print(lenny)
+#print(lenny)
 
 x = y.split(" ")
-print(x)
+#print(x)
 
 lenny2 = len(x)
 print(lenny2)
 
+print(type(x))
 
 for adam in range(0,lenny2):
     temp = x[adam]
+    #print(adam,temp)
     if temp.isdigit():
-        x[adam] = int(x[adam])
-    else:
-        print(temp)
+        alexander.append(int(x[adam])) 
+        #print("hshs")
+
+        
+lenny2 = len(alexander)      
+temp2 =  0  
+for chuck in range(0,lenny2):
+    temp2 = temp2 + int(alexander[chuck])
     
+temp2 = temp2 / lenny2
+mean = temp2
+
+
+print(lenny2,mean)
+
+
+ben_mcmenamin = lenny2 /2
+print(ben_mcmenamin)
+ben_mcmenamin = int(ben_mcmenamin)
+
+for chuck2 in range(0,lenny2):
+    if chuck2 == ben_mcmenamin:
+        temp3 = 
+    
+
+
+    
+
 
     
         
